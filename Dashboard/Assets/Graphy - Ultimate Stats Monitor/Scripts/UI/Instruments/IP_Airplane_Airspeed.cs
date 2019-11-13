7000d4fc5debad97bf30514018015c8464204ec9
Sergio Mesa <media@qbo3d.com>
2019-11-12 23:14:29 -0500

    Act

--- Dashboard/Assets/Graphy - Ultimate Stats Monitor/Scripts/UI/Instruments/IP_Airplane_Airspeed.cs
+++ Dashboard/Assets/Graphy - Ultimate Stats Monitor/Scripts/UI/Instruments/IP_Airplane_Airspeed.cs
@@ -25,7 +25,7 @@ namespace IndiePixel
             {
                 //float currentKnots = characteristics.MPH * mphToKnts;
                 float currentKnots = MPH * mphToKnts;
-                Debug.Log(currentKnots);
+                //Debug.Log(currentKnots);
 
                 float normalizedKnots = Mathf.InverseLerp(0f, maxIndicatedKnots, currentKnots);
                 float wantedRotation = 360f * normalizedKnots;
